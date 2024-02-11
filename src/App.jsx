9e6a809d60366c18372bf0e8a7cad253b4dda8d8
v-magiri <ndung'u.victor19@students.dkut.ac.ke>
2024-02-11 19:09:41 +0300

    :tada:Refactor Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
     <>
       <BrowserRouter>
         <NavbarComponent/>  
-
       </BrowserRouter>
     </>
   )
--- src/components/navbarComponent.jsx
+++ src/components/navbarComponent.jsx
@@ -1,20 +1,24 @@
-import {victorLogo} from '/assets/victor.svg';
+import React from 'react';
+import logo from '../../public/assets/victor.svg';
+import vlogo from '../../public/assets/logo.png';
+import '../styles/navbar.css';
 
-const NavbarComponent=() =>{
-    return(
-        <nav>
-            <div className="logo">
-                <Link to={"/"}><img src={victorLogo} className='logo' alt="Logo" /></Link>
-            </div>
-            <div className="menu">
-                <ul>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Projects</a></li>
-                    <li><a href="">Blog</a></li>
-                </ul>
-            </div>
-        </nav>
-    );
+const NavbarComponent = () => {
+  return (
+    <nav className='navbar'>
+        <img src={vlogo} alt="Logo"  className='logo'/>
+        <div className="menu">
+            <ul>
+                <li><a href="">About</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Blog</a></li>
+            </ul>
+        </div>
+        <div className="cta">
+            <button>Contact Me</button>
+        </div>
+    </nav>
+  )
 }
 
 export default NavbarComponent;
