b6c9914b812fb8ac54efb2a95cb84f1611c6d42d
v-magiri <ndung'u.victor19@students.dkut.ac.ke>
2024-02-13 20:58:32 +0300

    :zap: Add Projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,32 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import project_1 from '../assets/project_1.png';
 import project_2 from '../assets/project_2.png';
+import Project_2 from '../assets/project_2.svg';
 import project_3 from '../assets/project_3.png';
+import axios from 'axios';
+import '../styles/styles.css';
 
-const Projects = () => {
-  const [data,setData] = useState([]);
-
-  useEffect(()=>{
-    const fetchProjects=async()=>{
-      try {
-        const response=await fetch('../../assets/projects.json');
-        const projects=await response.json();
-        setData(projects);
-        console.log(projects);
-      } catch (error) {
-        console.error("Error fetching Project:",error);
-      }
-    }
-
-    fetchProjects();
-  },[]);
-  
+const Projects = () => {  
   return (
-    <section id='projects' className='projectSect'>
+    <section id='projects' className='projectSect container'>
       <h4 className='text-center my-3'>What I have worked on:</h4>
-      <div className="projectContainer">
-        <div className="projectCard">
-
+      <div className="projectContainer row">
+        <div className="projectCard col">
+            <div className="illustration">
+              <img src={project_1} alt="Project 1" className='projectIllustration' />
+            </div>
+            <div className="projectDesc">
+              <span className="projectTitle">AEUAFRICA Website </span>
+              <p>Website and CMS developed for the Alliance for Entreprenuerial Universities in Africa</p>
+              <a href="https://aeuafrica.org/"></a>
+            </div>
+        </div>
+        <div className="projectCard col">
+            <div className="illustration">
+              <img src={Project_2} alt="Project 1" className='projectIllustration' />
+            </div>
+            <div className="projectDesc">
+              <span>FAMALINK</span>
+              <p>An agritech solution aimed at helping farmers to make data driven farm budgeting and other farm decisions</p>
+              <a href=""></a>
+            </div>
+        </div>
+        <div className="projectCard col">
+            {/* <div className="illustration"> */}
+              <img src={project_3} alt="Project 1" className='projectIllustration' />
+            {/* </div> */}
+            <div className="projectDesc">
+              <span>K-Unity Transport Requisition Portal</span>
+              <p>A web solution designed for K-UNITY sacco for manage transport requisitions in the institution and manage fleet</p>
+              <a href=""></a>
+            </div>
         </div>
       </div>
     </section>
