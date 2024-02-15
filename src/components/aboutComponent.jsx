723313cb9933c06ff2930a2d303946df17d65f0d
v-magiri <ndung'u.victor19@students.dkut.ac.ke>
2024-02-15 21:57:29 +0300

    :lip: Add Responsiveness to Projects

--- src/components/aboutComponent.jsx
+++ src/components/aboutComponent.jsx
@@ -7,31 +7,27 @@ import { Link } from 'react-router-dom';
 const AboutComponent = () => {
   return (
     <section id='about' className='aboutSect'>
-      <h4 className='text-center'>Meet Victor</h4>
-      <div className="aboutContainer">
-          <div className="profileContainer">
-              <img src={profile} alt="Profile Image" className='profileImg' />
-          </div>
-        <div className="summaryContainer">
-          <p>
-            I'm a Full Stack developer in Kenya, blending a strong computer science background
-             with over two years of industry experience to bring software ideas to life. Proficient 
-             across the development lifecycle, I excel in creating impactful solutions, from web 
-             applications to backend systems.</p>
-          <p>
-            I am committed to staying ahead in technology, I strive to 
-             make a positive impact by delivering innovative software solutions that empower individuals 
-             and organizations in Kenya and beyond.
-          </p>
-          <p>Do you have a software idea you would like implemented? I’m here for you!</p>
-          <div className=''>
-            <Link to='https://drive.google.com/file/d/1cwtSK38YjjRfccl6MPj3Dl1K3q8ZzYYK/view?usp=drive_link' className='downloadBtn'>
-              <FontAwesomeIcon icon={faCloudArrowDown}/>
-              <span className='downloadTitle'>Download Resume</span>
-            </Link>
-          </div>
+      <h4 className='text-center aboutTitle'>Meet Victor</h4>
+      <img src={profile} alt="Profile Image" className='profileImg' />
+      <div className="summaryContainer mt-3">
+        <p>
+          I'm a Full Stack developer in Kenya, blending a strong computer science background
+            with over two years of industry experience to bring software ideas to life. Proficient 
+            across the development lifecycle, I excel in creating impactful solutions, from web 
+            applications to backend systems.
+        </p>
+        <p>
+          I am committed to staying ahead in technology, I strive to 
+            make a positive impact by delivering innovative software solutions that empower individuals 
+            and organizations in Kenya and beyond.
+        </p>
+        <p>Do you have a software idea you would like implemented? I’m here for you!</p>
+        <div className='aboutAction'>
+          <Link to='https://drive.google.com/file/d/1cwtSK38YjjRfccl6MPj3Dl1K3q8ZzYYK/view?usp=drive_link' className='downloadBtn'>
+            <FontAwesomeIcon icon={faCloudArrowDown}/>
+            <span className='downloadTitle'>Download Resume</span>
+          </Link>
         </div>
-
       </div>
     </section>
   )
